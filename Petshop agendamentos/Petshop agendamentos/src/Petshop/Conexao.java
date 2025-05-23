e3a376a75a6c109e97368c6722882e0a1df764c8
joaomatos3005 <148239450+joaomatos3005@users.noreply.github.com>
2025-05-23 00:17:23 -0300

    Projeto ok
    Projeto atualizado.

---
+++ Petshop agendamentos/Petshop agendamentos/src/Petshop/Conexao.java
@@ -0,0 +1,16 @@
+package petshop;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    public static Connection conectar() {
+        try {
+            return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/petshop", "root", "12345678"
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
